68c8591addbd98c94505ea133f4348afd1226390
Umma Ahimsha <uahimsha@gmail.com>
2020-10-28 16:45:23 +0700

    added form validation

--- components/FormComments.js
+++ components/FormComments.js
@@ -1,28 +1,49 @@
-import React from "react"
+import React, { useState } from "react"
 import { useRouter } from "next/router"
+import { useForm } from "react-hook-form"
 
 export default function FormComments() {
+  // Form Validation
+  const { register, handleSubmit, errors } = useForm()
+  const onSubmit = (data) => console.log(data)
   const router = useRouter()
-
-  const handleSubmit = (e) => {
-    router.push("/")
-  }
   return (
     <>
-      <form onSubmit={handleSubmit} noValidate className="form-comment">
+      <form onSubmit={handleSubmit(onSubmit)} className="form-comment">
         <fieldset>
           <legend>
             <h3>Tambahkan komentar</h3>
           </legend>
-          <input type="text" placeholder="nama" required />
-          <input type="email" placeholder="email@domain.com" required />
-          <textarea placeholder="tambahkan komentarmu" required />
+          <input
+            name="namaRequired"
+            placeholder="nama"
+            ref={register({ required: true })}
+          />
 
+          {errors.namaRequired && (
+            <small className="errors">Harap masukan nama</small>
+          )}
+          <input
+            name="emailRequired"
+            type="email"
+            placeholder="email@domain.com"
+            ref={register({ required: true })}
+          />
+
+          {errors.emailRequired && (
+            <small className="errors">Harap masukan email</small>
+          )}
+          <textarea
+            name="komentarRequired"
+            placeholder="tambahkan komentarmu"
+            ref={register({ required: true })}
+          />
+          {errors.komentarRequired && (
+            <small className="errors">Komentar tidak boleh kosong</small>
+          )}
           <div className="flex">
-            <button className="btn">Reset</button>
-            <button type="submit" className="btn btn-brown">
-              Submit
-            </button>
+            <input className="btn" type="reset" />
+            <input className="btn btn-brown" type="submit" />
           </div>
         </fieldset>
       </form>
--- components/Modal.js
+++ components/Modal.js
@@ -1,33 +1,51 @@
 import React from "react"
+import { useForm } from "react-hook-form"
 
 export default function Modal({ openModal, setOpenModal }) {
+  // Form Validation
+  const { register, handleSubmit, errors } = useForm()
+  const onSubmit = (data) => console.log(data)
+
+  // Handle toggle modal
   const handleModal = () => {
     setOpenModal(!openModal)
   }
   return (
     <div style={{ display: openModal ? `block` : `none` }} className="modal">
-      <form className="modal-content animate" action="">
+      <form className="modal-content animate" onSubmit={handleSubmit(onSubmit)}>
         <h2>Login</h2>
         <span onClick={handleModal} className="close" title="Close Modal">
           &times;
         </span>
         <div className="modal-container">
           <label htmlFor="email">
-            <b>Username</b>
+            <b>Email</b>
           </label>
-          <input type="email" placeholder="Enter email" name="email" required />
+          {errors.emailRequired && (
+            <small className="errors-login">Harap masukan email</small>
+          )}
+
+          <input
+            name="emailRequired"
+            type="email"
+            placeholder="Enter email"
+            ref={register({ required: true })}
+          />
 
           <label htmlFor="psw">
             <b>Password</b>
           </label>
+          {errors.passRequired && (
+            <small className="errors-login">Harap masukan password</small>
+          )}
           <input
+            name="passRequired"
             type="password"
-            placeholder="Enter Password"
-            name="psw"
-            required
+            placeholder="Enter email"
+            ref={register({ required: true })}
           />
 
-          <button type="submit">Login</button>
+          <input className="btn btn-brown" type="submit" />
         </div>
       </form>
     </div>
